4fbbd5042c172381e1f0e9a8576344236950efc6
Bijay Pathak <bkpathak@mtu.edu>
2015-10-20 20:51:06 -0400

    new programs

---
+++ IK/recursion/array_element_sum.py
@@ -0,0 +1,19 @@
+"""
+Check if the array contains elements which adds up to the target.
+[1,4,7,2] ,10 => 1 + 7 + 2
+"""
+
+def arr_element_sum(arr,target):
+    arr_element_sum(arr,target,0)
+
+def arr_element_sum(arr,target,start):
+    if target == 0:
+        return True
+    if start == len(arr):
+        return False
+
+    if arr_element_sum(arr,target - arr[start],start + 1 ):
+        return True
+    return arr_element_sum(arr,target,start + 1)
+
+print(arr_element_sum([1,4,7,2],10))
