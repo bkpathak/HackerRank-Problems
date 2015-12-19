3fee3e90319e604920a8feb2889fefbceefb071d
Bijay Pathak <bkpathak@mtu.edu>
2015-12-19 13:29:39 -0800

    fixed the error

--- IK/ad-hoc/skyline.py
+++ IK/ad-hoc/skyline.py
@@ -26,63 +26,80 @@ class Strip(object):
         self.left = left
         self.hi = hi
 
+    def __str__(self):
+        return ("({0},{1})".format(self.left,self.hi))
+
+
 class Skyline(object):
     """
     Creates the Skyline from Buildings.
     """
-    def __init__(self,silhouette = [] ):
-        silhouette = silhouette
-        strip_count = 0
+    def __init__(self):
+        self.silhouette = []
+        self.strip_count = 0
 
     def add_to_silhourette(self,strip):
         # Strip is redundant if it has same height or left as previous
-        if strip_count > 0 and silhouette[-1].hi == strip.hi:
+        if self.strip_count > 0 and self.silhouette[-1].hi == strip.hi:
             return
-        if strip_count > 0 and silhouette[-1].left == strip.left:
-            silhouette[-1].ht = max(silhouette[-1].ht, strip.ht)
+        if self.strip_count > 0 and self.silhouette[-1].left == strip.left:
+            self.silhouette[-1].ht = max(self.silhouette[-1].ht, strip.ht)
 
-        silhouette.append(strip)
-        strip_count += 1
+        self.silhouette.append(strip)
+        self.strip_count += 1
 
     def find_silhoutte(self,building_arr, low, high):
         if (low == high):
             sk = Skyline()
-            sk.append(Strip(building_arr[low][0],building_arr[low][1]))
-            sk.append(Strip(building_arr[low][2],0))
+            sk.add_to_silhourette(Strip(building_arr[low][0],building_arr[low][1]))
+            sk.add_to_silhourette(Strip(building_arr[low][2],0))
             return sk
 
-        mid = low + (low - high) / 2
+        mid = low + (high - low) // 2
         sk_left = self.find_silhoutte(building_arr,low,mid)
         sk_right = self.find_silhoutte(building_arr,mid+1,high)
-        return self.merge(sk_left,sk_right,low,mid,high)
+        return self.merge(sk_left,sk_right)
 
     def displlay_silhoutte(self):
         for sh in self.silhouette:
-            print(sh)
-            
-    def merge(self,sk_left, sk_right, low,mid,high):
+            print(sh,end = ",")
+
+        print()
+
+    def merge(self,sk_left, sk_right):
         """
         Merge the two skyline
         """
-        hi1 , hi2 = 0
+        h1 , h2 = 0,0
         result = Skyline()
-        i = lo
-        j = mid + 1
-        for k in range(low,high+1):
-            if i > mid:
-                result.append(sk_right[j])
+        i = 0
+        j = 0
+        while i < sk_left.strip_count or j < sk_right.strip_count:
+            if i >= sk_left.strip_count:
+                result.add_to_silhourette(sk_right.silhouette[j])
                 j += 1
-            elif j > high:
-                result.append(sk_left[i])
+            elif j >= sk_right.strip_count:
+                result.add_to_silhourette(sk_left.silhouette[i])
                 i += 1
             # check for the x-coordinate
-            elif sk_left[k].left < sk_right[k].left:
-                h1 = sk_left[k].hi
+            elif sk_left.silhouette[i].left < sk_right.silhouette[j].left:
+                h1 = sk_left.silhouette[i].hi
                 mx_hi = max(h1,h2)
-                result.append(Strip(sk_left[k].left,mx_hi))
+                result.add_to_silhourette(Strip(sk_left.silhouette[i].left,mx_hi))
                 i += 1
             else:
-                h2 = sk_right[k].hi
+                h2 = sk_right.silhouette[j].hi
                 mx_hi = max(h1,h2)
-                result.append(Strip(sk_right[k].left,mx_hi))
+                result.add_to_silhourette(Strip(sk_right.silhouette[j].left,mx_hi))
                 j += 1
+
+        return result
+
+
+if __name__ == "__main__":
+    sk = Skyline()
+    building_array = [(1,11,5), (2,6,7), (3,13,9), (12,7,16), (14,3,25),
+         (19,18,22), (23,13,29), (24,4,28) ]
+
+    sky_line = sk.find_silhoutte(building_array,0, len(building_array) - 1)
+    sky_line.displlay_silhoutte()
