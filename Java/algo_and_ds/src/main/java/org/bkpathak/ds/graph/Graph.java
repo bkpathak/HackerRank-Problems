c9b691b368e9b9fbe385b88570f4119e9f4b65ae
Bijay Pathak <bkpathak@mtu.edu>
2016-02-06 14:59:32 -0800

    graph

--- Java/algo_and_ds/src/main/java/org/bkpathak/ds/graph/Graph.java
+++ Java/algo_and_ds/src/main/java/org/bkpathak/ds/graph/Graph.java
@@ -1,7 +1,48 @@
 package org.bkpathak.ds.graph;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
- * Created by bijay on 2/6/16.
+ * Simple Adjacency Graph implementation for used in Graph Algorithms
  */
-public class Graph {
+public class Graph{
+    int vertex; // No of vertices in graph
+    ArrayList<ArrayList<Integer>> adjacecyList; // Adjacency List
+
+    Graph(int v){
+        vertex = v;
+        adjacecyList = new ArrayList<ArrayList<Integer>>(v);
+        for(int i =0; i < v; i++){
+            adjacecyList.add(new ArrayList<Integer>());
+        }
+    }
+
+    /**
+     * Add edge to the vertex in graph
+     */
+    void addEdge(int v, int e){
+        adjacecyList.get(v).add(e);
+    }
+
+    /**
+     * Print the Graph Adjacency List representation
+     */
+    void printGraph(){
+        for(int i = 0; i < vertex; i++){
+            System.out.print("Vertex " + i +" : ");
+            for(int j = 0;j < adjacecyList.get(i).size(); j++){
+                System.out.print( adjacecyList.get(i).get(j) + " ");
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n");
+    }
+
+    /**
+     * Return the iterator for the graph list
+     */
+    public Iterator<Integer> iterator(int v){
+        return adjacecyList.get(v).listIterator();
+    }
 }
--- Java/algo_and_ds/src/main/java/org/bkpathak/ds/graph/GraphTraversal.java
+++ Java/algo_and_ds/src/main/java/org/bkpathak/ds/graph/GraphTraversal.java
@@ -1,12 +1,85 @@
 package org.bkpathak.ds.graph;
 
+import java.util.*;
+
 /**
  * Created by bijay on 2/6/16.
  * Depth First Implementation
  */
 public class GraphTraversal {
-    pu
-    public static void dfs(Graph graph){
+    boolean[] visited;
+
+    GraphTraversal(int v){
+        visited = new boolean[v];
+    }
+
+    /**
+     * DFS util function
+     */
+    public void dfsUtil(Graph graph,int v){
+        // Set the current vertex as visited
+        visited[v] = true;
+        // Print the current vertex.
+        System.out.print(v + " ");
+
+        // visit all the adjacent node of this vertex
+        Iterator<Integer> itr = graph.iterator(v);
+        while(itr.hasNext()){
+            int nextVertex = itr.next();
+            if (!visited[nextVertex]){
+                dfsUtil(graph,nextVertex);
+            }
+        }
+    }
+
+    public void dfs(Graph graph,int startVertex){
+        // clear the visited array
+        Arrays.fill(visited,false);
+        dfsUtil(graph,startVertex);
+    }
+
+    /**
+     * BFS search
+     */
+
+    public void bfs(Graph graph, int startV){
+        // Clear the visited array
+        Arrays.fill(visited,false);
+
+        // Queue for BFS
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+
+        // Add the first vertex to the queue
+        queue.add(startV);
+
+        // Mark the startV as visited
+        visited[startV] = true;
+        while(!queue.isEmpty()){
+            // Get the first element from the queue and print it
+            int currentV = queue.poll();
+            System.out.print(currentV + " ");
+            Iterator<Integer> itr = graph.iterator(currentV);
+
+            // Loop through all the adjacent vertices
+            while(itr.hasNext()){
+                
+            }
+        }
+
+    }
+
+    public static void main(String[] args){
+        Graph graph = new Graph(5);
+        GraphTraversal gTraverse = new GraphTraversal(5);
+        graph.addEdge(0,1);
+        graph.addEdge(0,4);
+        graph.addEdge(0,2);
+        graph.addEdge(1,3);
+        graph.addEdge(3,2);
+        graph.addEdge(4,2);
+        // DFS graph traverse
+        System.out.println("DFS traversal of graph starting at the vertex " + 0);
+        gTraverse.dfs(graph,0);
 
     }
 }
