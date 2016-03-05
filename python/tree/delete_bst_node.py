63d4f49b7a0b09628018e163fba8c5aacdf15ace
Bijay Pathak <bkpathak@mtu.edu>
2016-03-04 22:38:54 -0800

    delete the node from BST

---
+++ python/tree/delete_bst_node.py
@@ -0,0 +1,6 @@
+# Delete the node from the tree
+# Approach
+# If node is leaf Node: Detach the node and update the deletd parent child with
+# null
+# Node to be deleted with only one Node: Connect the parent with chile and
+# remove the node
